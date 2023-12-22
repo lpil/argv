c293aca81e5222614cfd7d2a26a2d5ed4061fd4d
Louis Pilfold <louis@lpil.uk>
2023-12-22 19:04:57 +0000

    Document

--- src/argv_ffi.erl
+++ src/argv_ffi.erl
@@ -6,9 +6,12 @@ load() ->
     Runtime = <<(stringarg(bindir))/binary, "/", (stringarg(progname))/binary>>,
     PlainArguments = lists:map(fun list_to_binary/1, init:get_plain_arguments()),
     {Program, Arguments} = case init:get_argument(escript) of
+        % We're in an escript, so the first argument is the executable name
         {ok, _} ->
             [P | Rest] = PlainArguments,
             {P, Rest};
+
+        % We're not in a escript. Assume the cwd is the project root.
         _ ->
             {ok, Cwd} = file:get_cwd(),
             {list_to_binary(Cwd), PlainArguments}
