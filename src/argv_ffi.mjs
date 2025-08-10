70031b6b8338bd950038d4ae0b5e1c7f308d2f67
KITAGAWA Yasutaka <kit494way@gmail.com>
2025-08-10 11:24:07 +0900

    Fix arguments when using `-e` with Node.js or Bun

--- src/argv_ffi.mjs
+++ src/argv_ffi.mjs
@@ -1,16 +1,24 @@
 import { List } from "./gleam.mjs";
 
 export function load() {
-  if (globalThis.process) {
-    const [runtime, program, ...args] = process.argv;
-    return [runtime, program, List.fromArray(args)];
-  }
-
   if (globalThis.Deno) {
     const runtime = Deno.execPath();
     const program = new URL(Deno.mainModule).pathname;
     const args = List.fromArray(Deno.args);
     return [runtime, program, args];
+  }
+
+  if (globalThis.process) {
+    const specialArgs = ["-e", "--eval", "-p", "--print"];
+    // In Node.js and Bun, when special arguments are specified, `process.argv[1]` is not a script path,
+    // and command line arguments are stored starting from `process.argv[1]`.
+    if (process.execArgv.some((x) => specialArgs.includes(x))) {
+      const [runtime, ...args] = process.argv;
+      return [runtime, "", List.fromArray(args)];
+    } else {
+      const [runtime, program, ...args] = process.argv;
+      return [runtime, program, List.fromArray(args)];
+    }
   }
 
   const runtime = "browser";
