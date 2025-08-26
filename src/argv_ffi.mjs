8aecffa31e01e0ab04051cb3955406e431fe1c4b
KITAGAWA Yasutaka <kit494way@gmail.com>
2025-08-26 22:54:58 +0900

    Wrap comment

--- src/argv_ffi.mjs
+++ src/argv_ffi.mjs
@@ -10,8 +10,9 @@ export function load() {
 
   if (globalThis.process) {
     const specialArgs = ["-e", "--eval", "-p", "--print"];
-    // In Node.js and Bun, when special arguments are specified, `process.argv[1]` is not a script path,
-    // and command line arguments are stored starting from `process.argv[1]`.
+    // In Node.js and Bun, when special arguments are specified,
+    // `process.argv[1]` is not a script path, and command line arguments
+    // are stored starting from `process.argv[1]`.
     if (process.execArgv.some((x) => specialArgs.includes(x))) {
       const [runtime, ...args] = process.argv;
       return [runtime, "", List.fromArray(args)];
